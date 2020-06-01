44fc181cbf1aa222b8fc106ffbe9815f8c053631
SZIGETI János <szigeti.janos@pilar.hu>
2020-06-01 09:15:26 +0200

    factorial generalization

--- examples/factorial128.c
+++ examples/factorial128.c
@@ -9,18 +9,18 @@
   stdout: N lines where line i
   contains line index i and i!.
 
- Valid input interval: [1,98]
+ Valid input interval: [1,98] (512 bits)
 **********************************/
-#include "biguint512.h"
+#include "biguint128.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-#define STR_BUF_SIZE ((512 * 90) / 299 + 2)
+#define STR_BUF_SIZE ((128 * 90) / 299 + 2)
 
 int main(int argc, const char *argv[]) {
- BigUInt512 unit = biguint512_ctor_unit();
- BigUInt512 factor = unit;	// bit-level copy
- BigUInt512 prod = unit;	// bit-level copy
+ BigUInt128 unit = biguint128_ctor_unit();
+ BigUInt128 factor = unit;	// bit-level copy
+ BigUInt128 prod = unit;	// bit-level copy
  char str_buf[STR_BUF_SIZE];
  unsigned int N;
  
@@ -32,10 +32,10 @@ int main(int argc, const char *argv[]) {
  }
 
  for (unsigned int i=1; i<=N; ++i) {
-  prod = biguint512_mul(&prod, &factor);
-  factor = biguint512_add(&factor, &unit);
-  str_buf[biguint512_print_dec(&prod, str_buf, STR_BUF_SIZE)]=0;
-  printf("%3d: %*s\n", i, 72, str_buf);
+  prod = biguint128_mul(&prod, &factor);
+  factor = biguint128_add(&factor, &unit);
+  str_buf[biguint128_print_dec(&prod, str_buf, STR_BUF_SIZE)]=0;
+  printf("%3u: %*s\n", i, 72, str_buf);
  }
 
  return 0;
