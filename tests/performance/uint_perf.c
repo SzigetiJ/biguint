b6cc19ffce127f1997aa0fbb1bf005936cd9b0ab
SZIGETI János <szigeti@tmit.bme.hu>
2023-12-15 19:44:04 +0100

    [CODEQUALITY]: uint_perf tests on different add / sub functions

--- tests/performance/uint_perf.c
+++ tests/performance/uint_perf.c
@@ -17,122 +17,133 @@
 #define LOOPS (1<<28) 	// 256M loops
 #define BUFSIZE 2048
 
+typedef enum {
+ ADD,
+ ADD2,
+ ADD_ASSIGN,
+ ADD_ASSIGN2,
+ SUB,
+ SUB2,
+ SUB_ASSIGN,
+ SUB_ASSIGN2
+} UintFunction;
+
+static inline UInt _uint_add2(UInt a, UInt b, buint_bool *carry) {
+ UInt c = a + b;
+ if (*carry) ++c;
+ *carry = *carry ? c <= a : c < a;
+ return c;
+}
+
+static inline UInt _uint_sub2(UInt a, UInt b, buint_bool *carry) {
+ UInt c = a - b;
+ if (*carry) --c;
+ *carry = *carry ? a <= c : a < c;
+ return c;
+}
+
+static inline UInt *_uint_add_assign2(UInt *a, UInt b, buint_bool *carry) {
+ *a += b;
+ if (*carry) ++(*a);
+ *carry = (*carry ? *a <= b : *a < b);
+ return a;
+}
+
+static inline UInt *_uint_sub_assign(UInt *a, UInt b, buint_bool *carry) {
+ UInt orig = *a;
+ *a -= b;
+ if (*carry) --(*a);
+ *carry = (*carry ? orig <= *a : orig < *a);
+ return a;
+}
+
 static void print_result(clock_t t_begin, clock_t t_end, const char *op, int cnt) {
  clock_t dt = t_end - t_begin;
  fprintf(stdout, "=== %d %s() operations ===\n", cnt, op);
  fprintf(stdout, "Elapsed time: %ld us,\t%.1f op/s\n", dt, (1000000.0 * cnt) / dt);
 }
 
-
-static inline UInt _uint_add(UInt a, UInt b, buint_bool *carry) {
- UInt c = a + b + !!(*carry);
- *carry = *carry ? c <= a : c < a;
- return c;
-}
-
-static inline UInt *_uint_add_assign(UInt *a, UInt b, buint_bool *carry) {
-  *a += b + !!(*carry);
-  *carry = (*carry ? *a <= b : *a < b);
-  return a;
- }
-
-static inline UInt _uint_sub(UInt a, UInt b, buint_bool *carry) {
- UInt c = a - b - !!(*carry);
- *carry = *carry ? a <= c : a < c;
- return c;
-}
-
-
-int main(int argc, const char *argv[]) {
- uint32_t loop_cnt;
+static inline void exec_uint_test_(UintFunction fun, UInt buf_init, UInt buf_step) {
  clock_t t0, t1;
+ uint32_t loop_cnt;
+ UInt sum = (fun==SUB || fun==SUB2 || fun == SUB_ASSIGN || fun == SUB_ASSIGN2?-1:0);
+ buint_bool carry = false;
 
  // generate some numbers
  UInt num[BUFSIZE];
  for (unsigned int i = 0U; i < BUFSIZE; ++i) {
-  num[i] = i * 29;
+  num[i] = buf_init + i * buf_step;
  }
 
- {
-  UInt sum1 = 0U;
-  buint_bool carry1 = false;
-  t0 = clock();
+ t0 = clock();
+ if (fun == ADD) {
   for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   sum1 = _uint_add(sum1, num[loop_cnt % BUFSIZE], &carry1);
+   sum = uint_add(sum, num[loop_cnt % BUFSIZE], &carry);
   }
-  t1 = clock();
-  print_result(t0, t1, "inline uint_add", LOOPS);
-  sum1+=!!carry1;
-  printf("sum1: %" PRIuint "\n", sum1);
+ } else if (fun == ADD2) {
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   sum = _uint_add2(sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+ } else if (fun == ADD_ASSIGN) {
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   uint_add_assign(&sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+ } else if (fun == ADD_ASSIGN2) {
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   _uint_add_assign2(&sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+ } else if (fun == SUB){
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   sum = uint_sub(sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+  ++sum;	// give back the (-1) init.value
+  sum*=-1;
+ } else if (fun == SUB2){
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   sum = _uint_sub2(sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+  ++sum;	// give back the (-1) init.value
+  sum*=-1;
+ } else if (fun == SUB_ASSIGN){
+  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
+   _uint_sub_assign(&sum, num[loop_cnt % BUFSIZE], &carry);
+  }
+  ++sum;	// give back the (-1) init.value
+  sum*=-1;
+ } else {
  }
+ sum+=!!carry;
+ t1 = clock();
+ print_result(t0, t1,
+  fun==ADD?"uint_add":
+  fun==ADD2?"uint_add2":
+  fun==ADD_ASSIGN?"uint_add_assign":
+  fun==ADD_ASSIGN2?"uint_add_assign2":
+  fun==SUB?"uint_sub":
+  fun==SUB2?"uint_sub2":
+  fun==SUB_ASSIGN?"uint_sub_assign":
+  "uint_sub_assign2",
+  loop_cnt);
+ printf("sum: %" PRIuint "\n", sum);
+}
 
- {
-  UInt sum2 = 0U;
-  buint_bool carry2 = false;
-  t0 = clock();
-  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   _uint_add_assign(&sum2, num[loop_cnt % BUFSIZE], &carry2);
-  }
-  t1 = clock();
-  print_result(t0, t1, "inline uint_add_assign", LOOPS);
-  sum2+=!!carry2;
-  printf("sum2: %" PRIuint "\n", sum2);
- } 
 
- {
-  UInt sum3 = -1;	// (== MAX_UINT) start at the top to avoid carry bit set after the 1st call
-  buint_bool carry3 = false;
-  t0 = clock();
-  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   sum3=_uint_sub(sum3, num[loop_cnt % BUFSIZE], &carry3);
-  }
-  t1 = clock();
-  print_result(t0, t1, "inline uint_sub", LOOPS);
-  ++sum3;	// give back the (-1) init.value
-  sum3-=!!carry3;
-  printf("sum3: %" PRIuint "\n", -sum3);
+
+int main(int argc, const char *argv[]) {
+ UInt buf_init[]={0,((UInt)1<<(4*sizeof(UInt)))-(BUFSIZE/2)*29,-1};
+ UInt buf_step[]={29,29,-29};
+ const char *title[]={"low","medium","high"};
+
+ for (int i=0; i<3; ++i) {
+  fprintf(stderr, "*** uint functions on %s numbers ***\n", title[i]);
+  exec_uint_test_(SUB, buf_init[i], buf_step[i]);
+  exec_uint_test_(SUB2, buf_init[i], buf_step[i]);
+  exec_uint_test_(SUB_ASSIGN, buf_init[i], buf_step[i]);
+  exec_uint_test_(ADD, buf_init[i], buf_step[i]);
+  exec_uint_test_(ADD2, buf_init[i], buf_step[i]);
+  exec_uint_test_(ADD_ASSIGN, buf_init[i], buf_step[i]);
+  exec_uint_test_(ADD_ASSIGN2, buf_init[i], buf_step[i]);
  }
-
- {
-  UInt sum1 = 0U;
-  buint_bool carry1 = false;
-  t0 = clock();
-  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   sum1 = uint_add(sum1, num[loop_cnt % BUFSIZE], &carry1);
-  }
-  t1 = clock();
-  print_result(t0, t1, "uint_add", LOOPS);
-  sum1+=!!carry1;
-  printf("sum1: %" PRIuint "\n", sum1);
- }
-
- {
-  UInt sum2 = 0U;
-  buint_bool carry2 = false;
-  t0 = clock();
-  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   uint_add_assign(&sum2, num[loop_cnt % BUFSIZE], &carry2);
-  }
-  t1 = clock();
-  print_result(t0, t1, "uint_add_assign", LOOPS);
-  sum2+=!!carry2;
-  printf("sum2: %" PRIuint "\n", sum2);
- } 
-
- {
-  UInt sum3 = -1;	// (== MAX_UINT) start at the top to avoid carry bit set after the 1st call
-  buint_bool carry3 = false;
-  t0 = clock();
-  for (loop_cnt = 0; loop_cnt < LOOPS; ++loop_cnt) {
-   sum3=uint_sub(sum3, num[loop_cnt % BUFSIZE], &carry3);
-  }
-  t1 = clock();
-  print_result(t0, t1, "uint_sub", LOOPS);
-  ++sum3;	// give back the (-1) init.value
-  sum3-=!!carry3;
-  printf("sum3: %" PRIuint "\n", -sum3);
- }
-
  return 0;
 }
 
